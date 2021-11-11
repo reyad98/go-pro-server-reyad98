cd2868b2e5553ad3bd5eea83c5c388cf431b9320
reyad98 <alexmarfi2233@gmail.com>
2021-11-11 19:15:09 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+require('dotenv').config(); 
+const { MongoClient } = require('mongodb');
+ const ObjectId = require('mongodb').ObjectId;
+const port = process.env.PORT || 5000
+
+app.use(cors());
+app.use(express.json())
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.71ji5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+      await client.connect();
+        const database = client.db('homeservices');
+        const servicesCollection = database.collection('services')
+        const servicesCollection2 = database.collection('allservices')
+        //get api
+        app.get('/services',async(req,res)=>{
+           const cursor = servicesCollection.find({});
+           const services = await cursor.toArray()
+           res.send(services);
+        })
+         //get single services
+
+        app.get('/services/:id',async(req,res)=>{
+           const id = req.params.id;
+           const query ={_id: ObjectId(id)};
+           const service = await servicesCollection.findOne(query);
+           res.json(service);
+        })
+        //post api
+        app.post('/services',async(req,res)=>{
+        const service = req.body;
+        console.log('hit the post api',service);
+        
+           const result = await servicesCollection.insertOne(service);
+           console.log(result);
+           res.json(result);
+        })
+      
+        //all data show
+        app.get('/allservices',async(req,res)=>{
+          const cursor = servicesCollection2.find({});
+          const allservices = await cursor.toArray()
+          res.send(allservices);
+       })
+       //post api
+       app.post('/allservices',async(req,res)=>{
+       const allservice = req.body;
+       console.log('hit the post api',allservice);
+       
+          const result = await servicesCollection2.insertOne(allservice);
+          console.log(result);
+          res.json(result);
+       })
+
+        
+    
+        }
+    finally{
+    
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+  res.send('Hello action!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${port}`)
+})
